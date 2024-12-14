3628159cf4d0aee1f1a64380a071e6f7ebac48ce
Vicente <pouyio@gmail.com>
2024-12-14 14:03:52 +0100

    day 12 part 1

---
+++ day-12/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	Garden "github.com/pouyio/advent-of-code-2024/day-12/models"
+)
+
+func part1() {
+	// Open the file for reading
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	garden := Garden.NewGarden()
+
+	// Create a new Scanner for the file
+	scanner := bufio.NewScanner(file)
+	y := 0
+	for scanner.Scan() {
+		garden.OriginalGarden = append(garden.OriginalGarden, []string{})
+		for _, letter := range scanner.Text() {
+			garden.OriginalGarden[y] = append(garden.OriginalGarden[y], string(letter))
+		}
+		y++
+	}
+
+	for y, row := range garden.OriginalGarden {
+		for x, letter := range row {
+			areaIndex := garden.GetMapIndex([2]int{x, y})
+			if areaIndex == -1 {
+				garden.Areas[letter] = append(garden.Areas[letter], [][2]int{})
+				areaIndex = len(garden.Areas[letter]) - 1
+				garden.Flood([2]int{x, y}, letter, areaIndex)
+			}
+		}
+	}
+
+	totalPrice := 0
+	for key, letters := range garden.Areas {
+		for _, areas := range letters {
+			area := len(areas)
+			perimeter := Garden.CalculatePerimeter(areas)
+			totalPrice += area * perimeter
+			fmt.Printf("Price %s = %d * %d = %d\n", key, area, perimeter, area*perimeter)
+		}
+	}
+	fmt.Println("Total price", totalPrice)
+
+}
+
+func part2() {
+
+}
+
+func main() {
+	part1()
+	// part2()
+}
---
+++ day-12/models/garden.go
@@ -0,0 +1,98 @@
+package Garden
+
+import "slices"
+
+type Garden struct {
+	OriginalGarden [][]string
+	Areas          map[string][][][2]int
+}
+
+func NewGarden() *Garden {
+	return &Garden{OriginalGarden: [][]string{}, Areas: map[string][][][2]int{}}
+}
+
+func (g *Garden) Flood(p [2]int, originalLetter string, areaIndex int) {
+
+	currentLetter := g.OriginalGarden[p[1]][p[0]]
+	if currentLetter != originalLetter {
+		return
+	}
+
+	if g.GetMapIndex([2]int{p[0], p[1]}) > -1 {
+		return
+	}
+
+	g.Areas[originalLetter][areaIndex] = append(g.Areas[originalLetter][areaIndex], p)
+
+	if g.isNextPositionSameLetter(p, [2]int{0, -1}) {
+		g.Flood([2]int{p[0], p[1] - 1}, originalLetter, areaIndex)
+	}
+	if g.isNextPositionSameLetter(p, [2]int{0, 1}) {
+		g.Flood([2]int{p[0], p[1] + 1}, originalLetter, areaIndex)
+	}
+	if g.isNextPositionSameLetter(p, [2]int{-1, 0}) {
+		g.Flood([2]int{p[0] - 1, p[1]}, originalLetter, areaIndex)
+	}
+	if g.isNextPositionSameLetter(p, [2]int{1, 0}) {
+		g.Flood([2]int{p[0] + 1, p[1]}, originalLetter, areaIndex)
+	}
+}
+
+func (m *Garden) isNextPositionSameLetter(position [2]int, inc [2]int) bool {
+	if position[1] == 0 && inc[1] == -1 || position[1] == len(m.OriginalGarden)-1 && inc[1] == 1 || position[0] == 0 && inc[0] == -1 || position[0] == len(m.OriginalGarden[0])-1 && inc[0] == 1 {
+		// outside of the map
+		return false
+	}
+	if m.OriginalGarden[position[1]+inc[1]][position[0]+inc[0]] == m.OriginalGarden[position[1]][position[0]] {
+		return true
+	}
+	return false
+}
+
+func (g *Garden) GetMapIndex(p [2]int) int {
+	letter := g.OriginalGarden[p[1]][p[0]]
+	areas := g.Areas[letter]
+
+	for index, area := range areas {
+		found := slices.IndexFunc(area, func(point [2]int) bool {
+			return point[0] == p[0] && point[1] == p[1]
+		})
+
+		if found > -1 {
+			return index
+		}
+	}
+
+	return -1
+}
+
+func CalculatePerimeter(points [][2]int) int {
+	pointSet := make(map[[2]int]bool)
+
+	// Store all points in a set for quick lookup
+	for _, p := range points {
+		pointSet[[2]int{p[0], p[1]}] = true
+	}
+
+	// Define the 4 possible directions (neighbors)
+	directions := [][2]int{
+		{0, 1},  // up
+		{1, 0},  // right
+		{0, -1}, // down
+		{-1, 0}, // left
+	}
+
+	perimeter := 0
+
+	// For each point, check how many sides are exposed
+	for _, p := range points {
+		for _, d := range directions {
+			neighbor := [2]int{p[0] + d[0], p[1] + d[1]}
+			if !pointSet[neighbor] {
+				perimeter++
+			}
+		}
+	}
+
+	return perimeter
+}
